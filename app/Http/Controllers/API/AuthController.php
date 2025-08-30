0c70e2fb32f162523eba277fd209631311a860c1
ihsan-khan <ehsankhan0577@gmail.com>
2025-08-30 11:47:44 +0500

    feat: login api and authcontroller were added

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (auth()->attempt($credentials)) {
+            $user = auth()->user();
+            $token = $user->createToken('YourAppName')->plainTextToken;
+
+            return response()->json([
+                'token' => $token,
+                'user' => $user,
+            ]);
+        }
+
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+}
