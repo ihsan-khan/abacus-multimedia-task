a1fdd51c6cfabda5b4ebe549eb72aed61ef11fc7
ihsan-khan <ehsankhan0577@gmail.com>
2025-08-30 12:15:43 +0500

    feat: Order model and migartion were added

---
+++ app/Models/Order.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'cart_id',
+        'order_number',
+        'subtotal',
+        'tax',
+        'shipping',
+        'total',
+        'status',
+        'shipping_address',
+        'billing_address',
+        'customer_email',
+        'customer_phone',
+    ];
+}
